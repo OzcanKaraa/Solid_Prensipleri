296d064fa1f74a85d4a2cb603b7d169ca260cbd3
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-06-06 10:52:52 +0300

    Add project files.

---
+++ SOLID_Live/Program.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    class Customer
+    {
+        public string Name { get; set; }
+
+        //public bool ValidateName(string name)
+        //{
+        //    return !string.IsNullOrEmpty(name);
+        //}
+
+        //public void Greeting(string name)
+        //{
+        //    Console.WriteLine("Hello " + name);
+        //}
+
+        // Bu yukardaki yapı SOLID prensiplerinin S sine uymuyor
+
+        class CustomerValidator
+        {
+            // Görevi gelendeğişgenin boş olup olmadığını görmek
+            public bool ValidateName(string name)
+            {
+                return !string.IsNullOrEmpty(name);
+            }
+        }
+
+        class CustormerManager
+        {
+            // Karşılama mesajı..
+            public void Greeting(string name)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+        }
+    }
+}
