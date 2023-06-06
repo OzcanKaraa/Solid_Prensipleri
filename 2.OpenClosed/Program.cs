6c62c79987b6812d892ac75121f8a0fdb071b4a3
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-06-06 11:51:12 +0300

    Some changes

---
+++ 2.OpenClosed/Program.cs
@@ -0,0 +1,120 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    // Bir işyerinde hem fulltime çalışan ek olarak da part time çalışan personellerin olduğu düşünürsek...
+
+    //class FulltimeEmployee
+    //{
+
+    //    public void CalculateSalary()
+    //    {
+    //        // ......
+    //        //.....
+
+    //        Console.WriteLine("Fulltime Employee salary =...");
+    //    }
+    //}
+
+    //class ParttimeEmployee
+    //{
+    //    public void CalculateSalary()
+    //    {
+    //        // ......
+    //        //.....
+
+    //        Console.WriteLine("Parttime Employee salary =...");
+    //    }
+    //}
+
+    //class SupportEmployee
+    //{
+    //    public void CalculateSalary()
+    //    {
+    //        // ......
+    //        //.....
+
+    //        Console.WriteLine("Support Employee salary =...");
+    //    }
+    //}
+
+    //class SalaryCalculator
+    //{
+    //    public void CalculateSalary(string employeeType)
+    //    {
+
+    //        if (employeeType == "Full") 
+    //        {
+    //            new FulltimeEmployee().CalculateSalary();
+    //        }
+    //        else if (employeeType == "Support")
+    //        {
+    //            new SupportEmployee().CalculateSalary();
+    //        }
+    //        else
+    //        {
+    //            new ParttimeEmployee().CalculateSalary();
+    //        }
+            
+    //    }
+    //}
+
+    // Senaryo değiştiğinde...Mesela fulltime ve parttime çalışma şeklinin yanına bir destek çalışması personeli geldiğini düşünmüş olsak..
+
+    // Bu durumda SalaryCalculator metoduna bir ekleme yaptık.Yani değiştirdir. Ama O prensibine göre değişime kapalı olsun deniyordu. Kuralı ezdik...
+
+    // Çözümü : bir Interface tanımlayarak bu durumun önüne geçebiliriz..
+
+    interface IEmployee 
+    { 
+        void CalculateSalary();
+    }
+
+    class FulltimeEmployee : IEmployee
+    {
+        public void CalculateSalary()
+        {
+            //...
+
+            Console.WriteLine( "Fulltime Employee Salary = 999999..");
+        }
+    }
+
+    class ParttimeEmployee : IEmployee
+    {
+        public void CalculateSalary()
+        {
+            //...
+
+            Console.WriteLine("Parttime Employee Salary = 777777..");
+        }
+    }
+
+    class SupportEmployee : IEmployee
+    {
+        public void CalculateSalary()
+        {
+            //...
+
+            Console.WriteLine("Support Employee Salary = 666666..");
+        }
+    }
+
+    class SalaryCalculator
+    {
+        private readonly IEmployee _employee;
+
+        public SalaryCalculator(IEmployee employee)
+        {
+            _employee = employee;   
+        }
+        public void CalculateSalary()
+        {
+            _employee.CalculateSalary();
+        }
+
+
+    }
+}
---
+++ 3.LiskovSubstitution/Program.cs
@@ -0,0 +1,64 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Apple apple = new Orange();
+
+        //Console.WriteLine(apple.GetColor());
+
+        IFruit fruit = new Orange();
+
+        Console.WriteLine($"Portakalın rengi : {fruit.GetColor()}");
+
+        fruit = new Apple();
+        Console.WriteLine($"Elmanın rengi : {fruit.GetColor()}");
+
+
+        Console.ReadKey();
+    }
+
+    //public class Apple
+    //{
+    //    public virtual string GetColor()
+    //    {
+    //        return "Kırmızı";
+    //    }
+
+    //}
+
+    //// bu sınıf Apple sınıfından türemiş olsun
+    //public class Orange : Apple
+    //{
+    //    public virtual string GetColor()
+    //    {
+    //        return "Turuncu";
+    //    }
+    //}
+
+    // bu tür durumda kurtarıcımız yine Interface
+
+    public interface IFruit
+    {
+        string GetColor();
+    }
+
+    public class Apple : IFruit
+    {
+        public string GetColor()
+        {
+            return "Kırmızı";
+        }
+    }
+
+    public class Orange : IFruit
+    {
+        public string GetColor()
+        {
+            return "Turuncu";
+        }
+    }
+
+
+
+
+}
